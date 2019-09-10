658281a0638b53ed5111c98199649b44632b1832
Dzianis Spartak <dspartak@edmunds.com>
2019-09-10 18:40:51 +0300

    Added loading state

--- app/components/cards.js
+++ app/components/cards.js
@@ -31,13 +31,16 @@ export default Component.extend({
   isPerson: true,
   cardOneData: null,
   cardTwoData: null,
+  isLoading: false,
   actions: {
     async getCards() {
+      this.set('isLoading', true);
       const { cardOneData, cardTwoData } = await getNewCards(this.isPerson, this.gameData);
-
+      this.set('isLoading', false);
       setWinner(cardOneData, cardTwoData, this.isPerson);
 
       this.setProperties({ cardOneData, cardTwoData });
+
     },
     toggleType(isTypePerson) {
       this.set('isPerson', isTypePerson);
